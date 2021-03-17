041100daae0fc6d7747ee7eebcb3fd4197fcc2ca
unknown <manullang_d@yahoo.com>
2021-03-17 16:25:22 +0700

    dmdm

--- app.js
+++ app.js
@@ -180,7 +180,7 @@ class MyMain extends React.Component {
 			FormSearchstyle:{
 		display:"block"
 	},FormCategorystyle:{
-		display:"block"
+		display:"none"
 	},FormDetail:{
 		display:"none"
 	}
